742034367624b40d37b74cb776ec64ce4e4e43f4
Peter Halliday <pghalliday@gmail.com>
2015-11-27 16:27:49 +0100

    try to use ip address instead of hostname in proxy_pass

--- providers/proxy.rb
+++ providers/proxy.rb
@@ -4,26 +4,36 @@ end
 
 use_inline_resources
 
+def resolve_address(hostname)
+  ::Resolv.getaddress hostname
+rescue
+  hostname
+end
+
 action :create do
-  ssl_key = "/etc/nginx/ssl/#{new_resource.hostname}.key"
-  ssl_cert = "/etc/nginx/ssl/#{new_resource.hostname}.crt"
+  hostname = new_resource.hostname
+  port = new_resource.port
+  ip = resolve_address hostname
+  ssl_key = "/etc/nginx/ssl/#{hostname}.key"
+  ssl_cert = "/etc/nginx/ssl/#{hostname}.crt"
   file ssl_key do
     content new_resource.ssl_key
   end
   file ssl_cert do
     content new_resource.ssl_cert
   end
-  template "/etc/nginx/sites-available/#{new_resource.hostname}" do
+  template "/etc/nginx/sites-available/#{hostname}" do
     cookbook 'formatron_nginx'
     source 'proxy.erb'
     variables(
-      hostname: "#{new_resource.hostname}",
-      port: new_resource.port,
+      hostname: hostname,
+      ip: ip,
+      port: port,
       ssl_cert: ssl_cert,
       ssl_key: ssl_key
     )
   end
-  link "/etc/nginx/sites-enabled/#{new_resource.hostname}" do
-    to "/etc/nginx/sites-available/#{new_resource.hostname}"
+  link "/etc/nginx/sites-enabled/#{hostname}" do
+    to "/etc/nginx/sites-available/#{hostname}"
   end
 end
