c00bbd303a630d3bd79a8248e2d1922f2ba98008
Peter Halliday <pghalliday@gmail.com>
2015-12-08 17:25:57 +0100

    new log format to use with logstash

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,6 +6,10 @@ link '/etc/nginx/sites-enabled/default' do
   notifies :restart, 'service[nginx]', :delayed
 end
 
+cookbook_file '/etc/nginx/logstash.conf' do
+  notifies :restart, 'service[nginx]', :delayed
+end
+
 service 'nginx' do
   supports status: true, restart: true, reload: true
   action [:enable, :start]
